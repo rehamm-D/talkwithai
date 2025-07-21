8a8ccea816b5aeab1bef5fac20346ea12fa0f234
Tanisha Hasan <talkwithtinaa34@gmail.com>
2025-07-21 16:31:40 +0530

    Update index.js & connected with TALKwithAI

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
           name="description"
           content="TALKwithAI – a gentle yet powerful AI companion for smarter conversations."
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       {/*   Page Wrapper   */}
